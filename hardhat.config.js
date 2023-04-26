c3d441d10e66d62533924203d0692b11cf3ad49f
Hassan Sarfraz <hass4nkh4n09@gmail.com>
2023-04-26 11:33:27 +0500

    contract deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,29 @@
-require("@nomicfoundation/hardhat-toolbox");
+// hardhat.config.js
+const { mnemonic, bscscanApiKey } = require('./secrets.json');
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-ethers');
+require("@nomiclabs/hardhat-etherscan");
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
+
+  networks: {
+    testnet: {
+      url: `https://data-seed-prebsc-1-s1.binance.org:8545`,
+      accounts: { mnemonic: mnemonic }
+    },
+    mainnet: {
+      url: `https://bsc-dataseed.binance.org/`,
+      accounts: { mnemonic: mnemonic }
+    }
+  },
+
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://bscscan.com/
+    apiKey: bscscanApiKey
+  },
   solidity: {
     version: "0.8.13",
     settings: {
@@ -15,4 +37,4 @@ module.exports = {
       viaIR: true,
     },
   },
-}
+};
