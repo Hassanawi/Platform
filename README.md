cb4060892f709efc796eeb6dd0b6a1ecef9f4699
Hassan Sarfraz <hass4nkh4n09@gmail.com>
2023-04-27 11:20:28 +0500

    edited readme and deploy.js file

--- README.md
+++ README.md
@@ -68,3 +68,11 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+## Blockchain
+###  Contract Deployemnt
+
+Writer your wallet memonics and bsscanApikey in  .\secrets.json and run 
+
+'npx hardhat run .\scripts\deploy --network [specify network]'
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,12 +4,20 @@ async function main() {
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
-
   console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const ERC20Airdrop = await ethers.getContractFactory("ERC20Airdrop");
+  const erc20Airdrop = await ERC20Airdrop.deploy();
+
+  const PresaleContract = await ethers.getContractFactory("PresaleContract");
+  const presaleContract = await PresaleContract.deploy();
+
   const LiquidityLock = await ethers.getContractFactory("LiquidityLock"); 
   const liquidityLock = await LiquidityLock.deploy();
 
-  console.log("Token address:", liquidityLock.address);
+  console.log("ERC20Airdrop address:", erc20Airdrop.address);
+  console.log("PresaleContract address:", presaleContract.address);
+  console.log("LiquidityLock address:", liquidityLock.address);
 }
 
 main()
